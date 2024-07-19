a779b8eabf3ce382bb0d6f6eb80911fedb32893d
petaltank <petaltankstudios@gmail.com>
2024-07-18 20:20:00 -0600

    updated indicators

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,7 @@ static TRAY_MENU_ITEM_QUIT_DISPLAY: &str = "Quit Glucmon Completely";
 
 use config_data::initialize_config_data;
 use nightscout::get_glucose_data;
+use crate::nightscout::Direction;
 use std::thread;
 use tauri::{
     CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem
@@ -19,7 +20,7 @@ mod nightscout;
 use tauri::Icon;
 
 fn create_icon_from_path(path: &str) -> Result<Icon, Box<dyn std::error::Error>> {
-    let image = image::open(path)?.to_rgba();
+    let image = image::open(path)?.to_rgba8();
     let (width, height) = image.dimensions();
     let resized_image = image::imageops::resize(&image, width / 2, height / 2, image::imageops::FilterType::Nearest);
     Ok(Icon::Rgba {
@@ -29,26 +30,50 @@ fn create_icon_from_path(path: &str) -> Result<Icon, Box<dyn std::error::Error>>
     })
 }
 
-fn get_icon_path_from_direction(direction: &str) -> String {
-    match direction {
-        "Flat" => "icons/tray/glucmon_icon_FLAT.png".to_string(),
-        "FortyFiveUp" => "icons/tray/glucmon_icon_FORTYFIVE-UP.png".to_string(),
-        "FortyFiveDown" => "icons/tray/glucmon_icon_FORTYFIVE-DOWN.png".to_string(),
-        "SingleUp" => "icons/tray/glucmon_icon_SINGLE-UP.png".to_string(),
-        "SingleDown" => "icons/tray/glucmon_icon_SINGLE-DOWN.png".to_string(),
-        "DoubleUp" => "icons/tray/glucmon_icon_DOUBLE-UP.png".to_string(),
-        "DoubleDown" => "icons/tray/glucmon_icon_DOUBLE-DOWN.png".to_string(),
-        "TripleUp" => "icons/tray/glucmon_icon_TRIPLE-UP.png".to_string(),
-        "TripleDown" => "icons/tray/glucmon_icon_TRIPLE-DOWN.png".to_string(),
-        "RATE OUT OF RANGE" => "icons/tray/glucmon_icon_NOT-WORKING.png".to_string(),
-        "NOT COMPUTABLE" => "icons/tray/glucmon_icon_NOT-WORKING.png".to_string(),
-        "NONE" => "icons/tray/glucmon_icon.png".to_string(),
-        _ => "icons/tray/glucmon_icon.png".to_string(),
-    }
+fn get_icon_path_from_direction(direction: &Direction, glucose_value: &String) -> String {
+    let base_path = "icons/tray/";
+    
+    // Parse the glucose_value string to f64
+    let parts: Vec<&str> = glucose_value.split_whitespace().collect();
+    let glucose_str = parts[0];
+    let glucose_f64 = match glucose_str.parse::<f64>() {
+        Ok(value) => value,
+        Err(_) => return format!("{}glucmon_icon_NOT-CONFIGURED.png", base_path),
+    };
+    // println!("{} - {}", glucose_str, glucose_f64);
+    
+    let severity = match glucose_f64 {
+        v if v < 3.0 => "urgent",
+        v if v < 3.885 => "concern",
+        v if v < 10.0 => "normal",
+        v if v < 13.875 => "concern",
+        _ => "urgent",
+    };
+
+    // println!("{} - {}", glucose_str, severity);
+    
+
+    let direction_str = match direction {
+        Direction::Flat => "flat",
+        Direction::FortyFiveUp => "1_up", // "fortyfive_up", 
+        Direction::FortyFiveDown => "1_down", // "fortyfive_down",
+        Direction::SingleUp => "1_up",
+        Direction::SingleDown => "1_down",
+        Direction::DoubleUp => "2_up",
+        Direction::DoubleDown => "2_down",
+        Direction::TripleUp => "3_up",
+        Direction::TripleDown => "3_down",
+        Direction::RateOutOfRange => return format!("{}glucmon_icon_NOT-WORKING.png", base_path),
+        Direction::NotComputable => return format!("{}glucmon_icon_NOT-WORKING.png", base_path),
+        Direction::None => return format!("{}glucmon_icon.png", base_path),
+    };
+    println!("{}tray_{}_{}.png", base_path, severity, direction_str);
+
+    format!("{}tray_{}_{}.png", base_path, severity, direction_str).to_string()
 }
 fn main() {
     let (glucose_value_str, direction) = get_glucose_data().unwrap();
-    let tray_menu_glucose_data_item = CustomMenuItem::new(TRAY_MENU_ITEM_GLUCOSE_ENTRY, glucose_value_str);
+    let tray_menu_glucose_data_item = CustomMenuItem::new(TRAY_MENU_ITEM_GLUCOSE_ENTRY, &glucose_value_str);
     let tray_menu_quite_item =
         CustomMenuItem::new(TRAY_MENU_ITEM_QUIT_ENTRY, TRAY_MENU_ITEM_QUIT_DISPLAY);
     let tray_menu = SystemTrayMenu::new()
@@ -66,7 +91,7 @@ fn main() {
             let handle = app.handle();
             initialize_config_data(app).unwrap();
 
-            let icon_path = get_icon_path_from_direction(&direction);
+            let icon_path = get_icon_path_from_direction(&direction, &glucose_value_str);
             let icon = create_icon_from_path(&icon_path).unwrap();
             handle.tray_handle().set_icon(icon).unwrap();
             
@@ -82,7 +107,7 @@ fn main() {
                 //     "icons/glucmon_icon2.png"
                 // };
 
-                let icon_path = get_icon_path_from_direction(&direction);
+                let icon_path = get_icon_path_from_direction(&direction, &glucose_value_str);
                 let icon = create_icon_from_path(&icon_path).unwrap();
                 handle.tray_handle().set_icon(icon).unwrap();
                 item_handle.set_title(glucose_value_str).unwrap();
